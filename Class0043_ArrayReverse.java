f919d81b0fff55a6dd640f985e3b8dd012a48b21
ashwathnpj <ashwathnpj6@gmail.com>
2024-07-13 09:56:38 +0530

    all java codes

--- Class0043_ArrayReverse.java
+++ Class0043_ArrayReverse.java
@@ -1,16 +1,23 @@
 package java_package;
 //to reverse Array using iteration
 
+import java.util.Arrays;
+
 public class Class0043_ArrayReverse {
 
 	public static void main(String[] args) {
-		String company = "Google";
-		String output = "";
-		for (int i = company.length() - 1; i >= 0; i--) {
-			char c1 = company.charAt(i);
-			output = output + c1;
+		int [] age=new int[3];
+		age[0]=10;
+		age[1]=20;
+		age[2]=30;
+		
+		int [] output =new int[3];
+		for(int i=age.length-1;i>=0;i--) {
+			output[age.length - 1 - i]=age[i];
+			
 		}
-		System.out.println(output);
+		System.out.println("Our given array is --> "+Arrays.toString(age));
+		System.out.println("Reverse of the given array is --> "+Arrays.toString(output));
 	}
 
 }
