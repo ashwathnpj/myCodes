adb36a8ce0298f135d8be297e88a3206041cf302
ashwathnpj <ashwathnpj6@gmail.com>
2024-08-23 15:37:40 +0530

    Delete javaprograms directory

--- javaprograms/Class0023_Hierarchical5.java
+++
@@ -1,9 +0,0 @@
-package java_package;
-//hierarchical level inheritance concept using 5 classes
-
-public class Class0023_Hierarchical5 extends Class0023_Hierarchical1{
-	public static void main(String[] args) {
-		managername();
-		}
-
-}
