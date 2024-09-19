e7258d413a64dd09ee7c4aa782bc910e5577dbbe
ashwathnpj <ashwathnpj6@gmail.com>
2024-09-19 09:57:22 +0530

    Delete javaprograms directory

--- javaprograms/Class0022_Hierarchical1.java
+++
@@ -1,9 +0,0 @@
-package java_package;
-//hierarchical level inheritance concept using 2 classes
-
-public class Class0022_Hierarchical1 extends Class0022_Hierarchical{
-public static void main(String[] args) {
-		
-		manager();
-	}
-}
